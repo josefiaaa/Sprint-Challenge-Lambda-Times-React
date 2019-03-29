40fcd6198f137d5f6bdcb230d6e1dca06039229c
“Josefia <josefia.magagnoli@gmail.com>
2019-03-29 18:26:36 -0400

    Answers and styles

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -11,16 +11,27 @@ const Tab = props => {
   //   }
   // }
 
+ const activeTab = function (props) {
+  if (props.tab === props.selectedTab) {
+        return 'tab active-tab'
+      } else {
+        return 'tab'
+      }
+ }
+
+
+
   //don't know why that wasn't working but translated into ternary form so i could use active tab as the class name
 
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
 
-    const activeTab = props.tab === props.selectedTab ? 'tab active-tab' : 'tab';
+    // const activeTab = props.tab === props.selectedTab ? 'tab active-tab' : 'tab';
   return (
-    <div
-      className={activeTab}
+    <div 
+
+      className={activeTab(props)} 
       onClick={() => {
 
         props.selectedTab(props.tab)
